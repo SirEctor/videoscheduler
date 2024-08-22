61193c0cdae93e72e988b4527abaafbf95895928
SirEctor <ebenezer.teshome@duke.edu>
2024-08-22 14:11:14 -0400

    schedule page

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("INDEX JS LOADED!");
+let text = "potato";
+
+// $.ajax({
+//     type: "POST",
+//     url: "/videoscheduler/scrape.py",
+//     data: { param: text}
+//   }).done(function( o ) {
+//      // do something
+//      console.log("python finished, back in index.js");
+// });
